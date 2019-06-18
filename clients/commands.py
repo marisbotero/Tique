d0a2bcad0eaa7bd50f8ece9af84f4072536de2f0
marisbotero <maris2143@gmail.com>
2019-06-17 19:55:22 -0500

    💙

--- clients/commands.py
+++ clients/commands.py
@@ -6,18 +6,33 @@ def clients():
     """Maganes the clients Lifecycle """
     pass
 
+
+@click.command()
+@click.pass_context()
 def create(ctx,name,company,email,position):
     """Creates a new clients"""
     pass
 
+
+@click.command()
+@click.pass_context()
 def list(ctx):
     """List all clients"""
     pass
 
+
+@click.command()
+@click.pass_context()
 def update(client_uid):
     """Update a client"""
     pass
 
+
+@click.command()
+@click.pass_context()
 def delete(ctx):
     """Delete a client"""
     pass
+
+
+all = clients
